1988a41e899a2e1bb34de4dae42d2aaaf6be112a
shemplle <2693976547@qq.com>
2021-01-25 20:54:44 +0800

    redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react'
-import {Route ,Switch ,Redirect} from 'react-router-dom'
-import Login from './pages/login/login.jsx'
-import Admin from './pages/admin/admin.jsx'
-
 
 export default class App extends React.Component {
-  render () {
-    return (
-      <div className='app'>
-        <Switch>
-          <Route path='/login' component={Login} />
-          <Route path='/admin' component={Admin} />
-          <Redirect to='login' />
-        </Switch>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                App
+            </div>
+        )
+    }
 }
